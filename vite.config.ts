7649308ebef88f0f6e1a7b7cd4446330da65e1c3
matijamicunovic629 <micunovicmatija629@gmail.com>
2024-09-22 14:16:21 +0200

    [Fix] Fix PWA functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
@@ -27,7 +26,6 @@ export default defineConfig({
         ],
       },
     }),
-    react(),
     nodePolyfills(),
   ],
   build: {
